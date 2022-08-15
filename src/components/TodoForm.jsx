dbcaed0c7aed344d959294e8eab575f24d05c9cc
Anant-Singh1 <97743499+Anant-Singh1@users.noreply.github.com>
2022-08-15 18:12:21 +0530

    Create a form and add useState hook

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
+
+function TodoForm(props) {
+    const [input, setinput] = useState('');
+
+    const handleChange = e => {
+        setinput(e.target.value);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        // props.onSubmit({
+        //     id: nanoid(),
+        //     text: input
+        // });
+
+        setinput('');
+
+    };
+
+    return (
+        <form className="todo-form" onSubmit={handleSubmit}>
+            <input type="text" placeholder='Add a Todo' value={input}
+                className='todo-input' onChange={handleChange} />
+            <button className="todo-button">Add Todo</button>
+        </form>
+    )
+}
+
+export default TodoForm;
